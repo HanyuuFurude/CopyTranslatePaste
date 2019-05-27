ac435c682d9efe794dee0b32585656cfe1a8fb9c
HanyuuLu <HanyuuLu@outlook.com>
2019-05-27 13:24:43 +0800

    201905271324 初始ui重构完成，正在恢复功能能

--- ui.py
+++ ui.py
@@ -1,92 +1,131 @@
 import sys
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QLabel, QPlainTextEdit, QPushButton
-from PyQt5.uic import loadUi
-from PyQt5.QtCore import QCoreApplication, QTimer
+# from PyQt5 import *
+from PyQt5.QtWidgets import \
+	(
+		QWidget,
+		QMainWindow,
+		QPushButton,
+		QStatusBar,
+		QApplication,
+		QMessageBox,
+		QTextBrowser,
+		QGraphicsRectItem,
+		QCheckBox,
+		QTextEdit,
+		QLabel,
+		QVBoxLayout,
+		QGridLayout,
+	)
+from PyQt5.QtCore import Qt,QCoreApplication,QTimer
+import threading
+import time
+import datetime
 import t
-import logging
+class  MainUX(QMainWindow):
+	def __init__(self, parent=None, flags=Qt.WindowFlags()):
+		super().__init__(parent=parent, flags=flags)
+		self.title = '[dev] by: Hayuuu'
+		self.wgtCanvas = QWidget()
+		self.setCentralWidget(self.wgtCanvas)
+		self.autoRead = True
+		self.autoWrite = True
+		self.res=None	# 翻译结果储存变量
+		self.text=None	# 待翻译文本储存变量
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s %(filename)s]\n  line:%(lineno)d,level:%(levelname)s,message:%(message)s\n',
-    datefmt='%Y/%b/%d %H:%M:%S',
-    filename='log.log',
-    filemode='a'
-)
+		# 控件生成
+		self.qlbAutoRead = QLabel("自动读取内存文本")
+		self.qcbAutoRead = QCheckBox()
+		self.qlbAutoCopy = QLabel("自动将结果写入剪切板")
+		self.qcbAutoCopy = QCheckBox()
+		self.qlbInputLabel = QLabel("Input box")
+		self.qteInputText = QTextEdit()
+		self.qlbResultLabel = QLabel("result")
+		self.qteResultText = QTextEdit()
+		self.btnExit = QPushButton("退出")
+		self.timer = QTimer(self)	# 剪切板访问定时器
 
+		# 控件布局
+		## 设置区空间布局
+		self.wgtSettingArea = QWidget()
+		self.QltSettingLayout = QGridLayout()
+		self.QltSettingLayout.addWidget(self.qlbAutoRead,0,1)
+		self.QltSettingLayout.addWidget(self.qcbAutoRead,0,0)
+		self.QltSettingLayout.addWidget(self.qlbAutoCopy,1,1)
+		self.QltSettingLayout.addWidget(self.qcbAutoCopy,1,0)
+		self.wgtSettingArea.setLayout(self.QltSettingLayout)
+		## 主ui控件布局
+		self.qltMainLayout = QVBoxLayout()
+		self.qltMainLayout.addWidget(self.wgtSettingArea)
+		# self.qltMainLayout.addWidget(self.qlbAutoRead)
+		# self.qltMainLayout.addWidget(self.qcbAutoRead)
+		# self.qltMainLayout.addWidget(self.qlbAutoCopy)
+		# self.qltMainLayout.addWidget(self.qcbAutoCopy)
+		self.qltMainLayout.addWidget(self.qlbInputLabel)
+		self.qltMainLayout.addWidget(self.qteInputText)
+		self.qltMainLayout.addWidget(self.qlbResultLabel)
+		self.qltMainLayout.addWidget(self.qteResultText)
+		self.qltMainLayout.addWidget(self.btnExit)
+		self.wgtCanvas.setLayout(self.qltMainLayout)
+		self.setWindowTitle(self.title)
 
-class ui(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.res = None
-        self.text = None
-        # code to get and draw ui
-        self.autoRead = True
-        self.autoWrite = True
-        # self=QMainWindow()
-        loadUi('mainwindow.ui', self)
-        self.checkBoxR = self.findChild(QCheckBox, 'checkBoxR')
-        self.checkBoxR.stateChanged.connect(self.checkBoxChanged)
-        self.checkBoxW = self.findChild(QCheckBox, 'checkBoxW')
-        self.checkBoxW.stateChanged.connect(self.checkBoxChanged)
-        self.textRead = self.findChild(QPlainTextEdit, 'textOrigin')
-        self.textWrite = self.findChild(QPlainTextEdit, 'textTranslate')
-        self.buttonExit = self.findChild(QPushButton, 'buttonExit')
-        self.buttonExit.clicked.connect(
-            QCoreApplication.exit)  # button on exit
-        self.timer = QTimer(self)  # 初始化一个定时器
-        self.timer.timeout.connect(self.translate)  # 计时结束调用operate()方法
-        self.timer.start(1000)  # 设置计时间隔并启动
-        self.show()
+		# 功能绑定区
+		self.qcbAutoRead.stateChanged.connect(self.checkBoxChanged)
+		self.qcbAutoCopy.stateChanged.connect(self.checkBoxChanged)
+		self.btnExit.clicked.connect(QCoreApplication.exit)
+		self.timer.timeout.connect(self.translate)
+		self.timer.start(1000)
 
-    # checkBox function
-    def checkBoxChanged(self, state):
-        if self.sender() == self.checkBoxR:
-            self.autoRead = True if(state == 2) else False
-        elif self.sender() == self.checkBoxW:
-            self.autoWrite = True if (state == 2) else False
+		self.show()
+		return 
 
-    # fetch the text from textOrigin
-    def fetchPlainText(self):
-        return self.textRead.toPlainText()
+		
+	# checkBox function
+	def checkBoxChanged(self, state):
+		if self.sender() == self.qcbAutoRead:
+			self.autoRead = True if(state == 2) else False
+		elif self.sender() == self.qcbAutoCopy:
+			self.autoWrite = True if (state == 2) else False
 
-    # set the text to textTranslate
-    def setPlainText(self, text):
-        self.textWrite.clear()
-        self.textWrite.appendPlainText(text)
-        return text
+	# fetch the text from textOrigin
+	def fetchPlainText(self):
+		return self.qteInputText.toPlainText()
 
-    def translate(self):
-        if self.autoRead is True:
-            text = self.readClipboard()
-            if text is None or text == self.res:
-                return
-            self.textRead.clear()
-            self.textRead.appendPlainText(text)
-        else:
-            text = self.fetchPlainText()
-        if text == self.text:
-            return
-        else:
-            self.text = text
-        if text is None:
-            self.setPlainText('[empty]')
-            return
-        elif text == self.res:
-            return
-        self.res = self.setPlainText(t.translate(text))
-        self.setPlainText(self.res)
-        if self.autoWrite is True:
-            self.writeClipboard(self.res)
+	# set the text to textTranslate
+	def setPlainText(self, text):
+		self.qteResultText.clear()
+		self.qteResultText.appendPlainText(text)
+		return text
 
-    def readClipboard(self):
-        return t.gettext()
+	def translate(self):
+		if self.autoRead is True:
+			text = self.readClipboard()
+			if text is None or text == self.res:
+				return
+			self.qteInputText.clear()
+			self.qteInputText.appendPlainText(text)
+		else:
+			text = self.fetchPlainText()
+		if text == self.text:
+			return
+		else:
+			self.text = text
+		if text is None:
+			self.setPlainText('[empty]')
+			return
+		elif text == self.res:
+			return
+		self.res = self.setPlainText(t.translate(text))
+		self.setPlainText(self.res)
+		if self.autoWrite is True:
+			self.writeClipboard(self.res)
 
-    def writeClipboard(self, text):
-        t.settext(text)
+	def readClipboard(self):
+		return t.gettext()
 
+	def writeClipboard(self, text):
+		t.settext(text)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    run = ui()
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	mainUX=MainUX()
+	sys.exit(app.exec_())
