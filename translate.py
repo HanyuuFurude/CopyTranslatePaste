44c28be4791cc145123e57e8940ca0b975b7775c
HanyuuLu <HanyuuLu@outlook.com>
2019-06-22 00:08:40 +0800

    201906220008

--- translate.py
+++ translate.py
@@ -42,35 +42,24 @@ def sentence(res):
 def translate(queryString: str):
     head = \
         {
-            # "Host": "fanyi.baidu.com",
-            # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-            # "Accept": "*/*",
-            # "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-            # "Accept-Encoding": "gzip, deflate, br",
-            # "Referer": "https://fanyi.baidu.com/",
-            # "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            # "X-Requested-With": "XMLHttpRequest",
-            # "Connection": "keep-alive"
-            "Host":" fanyi.baidu.com",
-            "User-Agent":" Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0",
-            "Accept":" */*",
-            "Accept-Language":" zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-            "Accept-Encoding":" gzip, deflate, br",
-            "Referer":" https://fanyi.baidu.com/",
-            "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With":" XMLHttpRequest",
-            "Content-Length":" 121",
-            "Connection":" keep-alive",
-            "Cookie":" BAIDUID=C06A41C141B090CBD743BAD41E49F8DA:FG=1; BIDUPSID=C06A41C141B090CBD743BAD41E49F8DA; PSTM=1552839539; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1558795642,1558945603,1560824308; from_lang_often=%5B%7B%22value%22%3A%22dan%22%2C%22text%22%3A%22%u4E39%u9EA6%u8BED%22%7D%2C%7B%22value%22%3A%22hu%22%2C%22text%22%3A%22%u5308%u7259%u5229%u8BED%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; pgv_pvi=9974821888; locale=zh; pgv_si=s7580348416; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1560824308; ZD_ENTRY=bing; delPer=0; PSINO=1; H_PS_PSSID=26525_1466_21097_29064_28519_29098_28836_28584; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; yjs_js_security_passport=29d167a6439954367998f3579714aa0d6aa162be_1560824307_js",
-            "TE":" Trailers",
+            "Host": "fanyi.baidu.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+            "Accept": "*/*",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://fanyi.baidu.com/",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Requested-With": "XMLHttpRequest",
+            "Connection": "keep-alive",
+            "Cookie": "BAIDUID=C06A41C141B090CBD743BAD41E49F8DA:FG=1; BIDUPSID=C06A41C141B090CBD743BAD41E49F8DA; PSTM=1552839539; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1558795642,1558945603,1560824308,1561215629; from_lang_often=%5B%7B%22value%22%3A%22dan%22%2C%22text%22%3A%22%u4E39%u9EA6%u8BED%22%7D%2C%7B%22value%22%3A%22hu%22%2C%22text%22%3A%22%u5308%u7259%u5229%u8BED%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; pgv_pvi=9974821888; locale=zh; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; pgv_si=s7580348416; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1561215629; ZD_ENTRY=bing; delPer=0; PSINO=1; H_PS_PSSID=26525_1466_21097_29064_28519_29098_28836_28584; yjs_js_security_passport=38b6331e008ed02c90cb3ab7db7ed77909c317dd_1561215630_js%"
         }
     form = {
         "from": "en",
         "to": "zh",
         "query": queryString,
-        "source": "txt",
+        "transtype":"realtime",
         "simple_means_flag":3,
-        "sign":1721.439192,
+        # "sign":151721.439192,
         "token":"8adacf46fbcb4329dc03d0b85e2cbaab"
     }
     try:
